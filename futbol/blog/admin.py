7747de8a86dda1530472cf7c542ce4048a327f7f
nahum-oseguera26 <49953527+nahum-oseguera26@users.noreply.github.com>
2020-10-09 12:58:16 -0600

    Add files via upload

---
+++ futbol/blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import *
+
+
+class PublicacionAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'contenido', 'autor', 'f_pub')
+    readonly_fields = ['f_pub']
+    ordering = ['-f_pub'] 
+
+
+admin.site.register(Categoria)
+admin.site.register(Publicacion, PublicacionAdmin)
+
